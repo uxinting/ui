42ce512a8261bb6bacbeda14c4676cbe2daae844
wuxinting <wu.xinting@hotmail.com>
2013-05-29 21:42:50 +0800

    articles

---
+++ js/xinting.js
@@ -0,0 +1,24 @@
+!function($) {
+	$(function() {
+	
+		//Back to top icon functions
+		$(document).scroll(function() {
+			if ($(this).scrollTop() > 0) {
+				$('.icon-backtop').show();
+			} else {
+				$('.icon-backtop').hide();
+			}
+		});
+		
+		$('.icon-backtop').click(function() {
+			var id = setInterval(moveTop, 10);
+			function moveTop() {
+				$(document).scrollTop($(document).scrollTop() / 1.2);
+				if ($(document).scrollTop() < 1)
+					clearInterval(id);
+			}
+		});
+		
+		//tag p highlight
+	});
+}(window.jQuery);
